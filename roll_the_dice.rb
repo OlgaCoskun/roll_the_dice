98bf54fa28b9c1ea8431893adf0abc17e02a833d
Olga Coskun <ovin999@gmail.com>
2018-02-24 12:08:11 +0300

    Add puts last side

--- roll_the_dice.rb
+++ roll_the_dice.rb
@@ -19,12 +19,13 @@ puts "How many dice?"
 def show_rolling_die
   sum = 0 # Для подсчета суммы выпавших кубиков
   @number.times do
-    num = "#{rand(6) + 1}"
-    print num.to_i
-    sum += num.to_i
+    @num = "#{rand(6)}"
+    print @num.to_i
+    sum += @num.to_i
     sleep 0.1
     print "\r"
   end
   puts "Sum of dice #{sum}"
+  puts @num
 end
 show_rolling_die
